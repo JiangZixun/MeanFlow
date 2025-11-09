4460b2f768a3deb685625e0d4cf92a5134a560d1
jzx <657016777@qq.com>
2025-11-09 20:28:50 +0800

    1

--- train.sh
+++ train.sh
@@ -8,6 +8,7 @@ echo "Starting training Meanflow ..."
     --log_dir logs/UNet_1e-3_flowratio_1.0 \
     --batch_size 8 \
     --mode train \
-    --gpus 1
+    --gpus 1 \
+    --ckpt_path logs/UNet_1e-3_flowratio_1.0/checkpoints/step_255000-loss_0.3062.ckpt
 
 echo "Training finished."
