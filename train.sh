9bfa80c8179ddf7d702f012d20ff5ddead27f6ef
jzx <657016777@qq.com>
2025-11-24 15:58:03 +0800

    add ViT.

--- train.sh
+++ train.sh
@@ -21,11 +21,11 @@ echo "Starting training Meanflow ..."
 #     --gpus 1 
 
 # /root/anaconda3/envs/prediff/bin/python 
-/home/jzx/anaconda3/envs/torchcfm/bin/python train_JiU_RFDPIC.py \
-    --config configs/JiU_RFDPIC.yaml \
+/home/jzx/anaconda3/envs/torchcfm/bin/python train_JiT_RFDPIC.py \
+    --config configs/JiT_RFDPIC.yaml \
     --rfdpic_config configs/rfdpic_config.yaml \
     --rfdpic_ckpt pretrained_models/pretrained_RFDPIC_Dual_Rotation_Dyn.pt \
-    --log_dir logs/JiU_RFDPIC_1e-4_flowratio_1.0 \
+    --log_dir logs/JiT_RFDPIC_1e-4_flowratio_1.0 \
     --batch_size 8 \
     --mode train \
     --gpus 1 \
