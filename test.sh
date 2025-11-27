2044fe0a3df85a638ad68ee4b467a0d410b1a26f
jzx <657016777@qq.com>
2025-11-27 16:33:50 +0800

    add JiT from Gaussion Noise.

--- test.sh
+++ test.sh
@@ -3,32 +3,14 @@ cd /mnt/data1/MeanFlow/
 
 echo "Starting testing Meanflow ..."
 
-# /root/anaconda3/envs/prediff/bin/python 
-/home/jzx/anaconda3/envs/torchcfm/bin/python train_UNet_RFDPIC.py \
-    --config configs/UNet_RFDPIC.yaml \
+/home/jzx/anaconda3/envs/torchcfm/bin/python train_JiT_RFDPIC.py \
+    --config configs/JiT_RFDPIC.yaml \
     --rfdpic_config configs/rfdpic_config.yaml \
-    --rfdpic_ckpt "pretrained_models/pretrained_RFDPIC_Dual_Rotation_Dyn.pt" \
-    --ckpt_path "logs/UNet_RFDPIC_1e-4_flowratio_1.0/checkpoints/step_565000-loss_0.0916.ckpt" \
-    --log_dir logs/test \
-    --sample_steps 1000 \
+    --rfdpic_ckpt pretrained_models/pretrained_RFDPIC_Dual_Rotation_Dyn.pt \
+    --log_dir logs/JiT_RFDPIC_1e-4_flowratio_1.0 \
+    --sample_steps 50 \
     --batch_size 8 \
     --mode test \
     --gpus 1 \
-
-echo "Test finished."
-
-echo "Starting testing Meanflow ..."
-
-# /root/anaconda3/envs/prediff/bin/python 
-/home/jzx/anaconda3/envs/torchcfm/bin/python train_UNet_RFDPIC.py \
-    --config configs/UNet_RFDPIC.yaml \
-    --rfdpic_config configs/rfdpic_config.yaml \
-    --rfdpic_ckpt "pretrained_models/pretrained_RFDPIC_Dual_Rotation_Dyn.pt" \
-    --ckpt_path "logs/UNet_RFDPIC_1e-4_flowratio_1.0/checkpoints/step_975000-loss_0.0859.ckpt" \
-    --log_dir logs/test \
-    --sample_steps 1000 \
-    --batch_size 8 \
-    --mode test \
-    --gpus 1 \
-
-echo "Test finished."
+    --ckpt_path logs/JiT_RFDPIC_1e-4_flowratio_1.0/checkpoints/step_1000000-loss_0.1542.ckpt
+    # --use_wandb
